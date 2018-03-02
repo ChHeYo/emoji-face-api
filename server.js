c93ba5d756017eda64b2f4222e429559137c987f
ChHeYo <cyoo3@u.rochester.edu>
2018-03-01 21:35:07 -0500

    add heroku configuration

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const bcrypt = require('bcrypt');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : '',
-      password : '',
-      database : 'smartbrain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
